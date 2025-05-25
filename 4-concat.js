c754d4f79b04aa57d7b22d243c19de436c47d016
matshidisolp <mpitswane@gmail.com>
2025-05-25 22:36:55 +0700

    4-concat.js

---
+++ 4-concat.js
@@ -0,0 +1,8 @@
+const args = process.argv; // all command-line arguments
+
+// index 2 = first user command-line argument
+const arg1 = args[2];
+
+const arg2 = args[3];
+
+console.log(`${arg1} is ${arg2}`);
